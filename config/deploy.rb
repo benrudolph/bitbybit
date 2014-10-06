1fecbf8172d04f228c3b36349330a57127743f2b
Ben Rudolph <rudolphben@gmail.com>
2014-10-06 18:07:56 +0200

    Capistrano

---
+++ config/deploy.rb
@@ -0,0 +1,33 @@
+ssh_options[:forward_agent] = true
+require "bundler/capistrano"
+
+set :keep_releases, 5
+set :scm, :git
+set :scm_verbose, false
+
+# Set your repository URL
+set :repository, 'https://github.com/benrudolph/bitbybit.git'
+
+# Set your application name
+set :application, "bitbybit"
+set :deploy_via, :remote_cache
+
+# Set your machine user
+set :user, 'deploy'
+
+set :deploy_to, "/var/www/#{application}"
+set :use_sudo, false
+
+# Set your host, you can use the server IPs here if you don't have one yet
+role :app, 'personal', :primary => true
+
+default_run_options[:pty] = true
+
+namespace :octopress do
+  task :generate, :roles => :app do
+    run "cd #{release_path} && bundle exec rake generate"
+  end
+end
+
+after 'deploy:update_code', 'deploy:cleanup'
+after 'bundle:install', 'octopress:generate'
